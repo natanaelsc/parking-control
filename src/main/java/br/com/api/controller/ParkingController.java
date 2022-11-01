19355724160412d0f7a684ff97b318dc62f789bc
Natanael Silva <53065432+natanaelsc96@users.noreply.github.com>
2022-11-01 13:33:43 -0300

    add first endpoint

---
+++ src/main/java/br/com/api/controller/ParkingController.java
@@ -0,0 +1,32 @@
+package br.com.api.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.controller.dto.ParkingDTO;
+import br.com.api.controller.mapper.ParkingMapper;
+import br.com.api.model.Parking;
+import br.com.api.service.ParkingService;
+
+@RestController
+@RequestMapping("/parking")
+public class ParkingController {
+    
+    private final ParkingService parkingService;
+    private final ParkingMapper parkingMapper;
+
+    public ParkingController(ParkingService parkingService, ParkingMapper parkingMapper) {
+        this.parkingService = parkingService;
+        this.parkingMapper = parkingMapper;
+    }
+    
+    @GetMapping
+    public List<ParkingDTO> findAll() {
+        List<Parking> parkingList = parkingService.findAll();
+        List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
+        return result;
+    }
+}
